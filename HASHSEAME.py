9e2094fbe63259c296ac5d42155e66adc05aac7c
n0ipr0cs <n0ipr0cs@users.noreply.github.com>
2015-03-20 19:52:07 +0100

    Create HASHSEAME.py

---
+++ HASHSEAME.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# Creado by n0ipr0cs
+
+import getpass
+import hashlib
+import sys
+
+#creacion de las funciones de los hash
+def MD5_HASH():
+	passwd = getpass.getpass()
+	HASH_MD5 = hashlib.md5(passwd).hexdigest()
+	print HASH_MD5
+	return
+def SHA1_HASH():
+	passwd = getpass.getpass()
+	HASH_MD5 = hashlib.sha1(passwd).hexdigest()
+	print HASH_MD5
+	return
+def SHA224_HASH():
+	passwd = getpass.getpass()
+	HASH_MD5 = hashlib.sha224(passwd).hexdigest()
+	print HASH_MD5
+	return
+def SHA256_HASH():
+	passwd = getpass.getpass()
+	HASH_MD5 = hashlib.sha256(passwd).hexdigest()
+	print HASH_MD5
+	return
+def SHA384_HASH():
+	passwd = getpass.getpass()
+	HASH_MD5 = hashlib.sha384(passwd).hexdigest()
+	print HASH_MD5
+	return
+def SHA512_HASH():
+	passwd = getpass.getpass()
+	HASH_MD5 = hashlib.sha512(passwd).hexdigest()
+	print HASH_MD5
+	return
+
+print "************************************"
+print "H A S H S E A M E   by n0ipr0cs"
+print "************************************"
+
+
+
+
+print " 1. md5"
+print " 2 .sha1"
+print " 3 .sha224"
+print " 4 .sha256"
+print " 5 .sha384"
+print " 6 .sha512"
+
+VAR_1 = raw_input("Elige un algoritmo para tu password")
+
+if VAR_1 == "1":
+	MD5_HASH()
+elif VAR_1 == "2":
+	SHA1_HASH()
+elif VAR_1 == "3":
+	SHA224_HASH()
+elif VAR_1 == "4":
+	SHA256_HASH()
+elif VAR_1 == "5":
+	SHA384_HASH()
+elif VAR_1 == "6":
+	SHA512_HASH()
+else:
+	print "ERROR!!!  Elige una opcion valida"
+sys.exit(1)
+
+
+
+sys.exit(0)
